590c2184dc2e0a9fdaaeed82a9f1b80582333eb2
Samirah Lima <116299909+smrlm@users.noreply.github.com>
2023-03-09 11:06:44 -0300

    delete function change

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -14,6 +14,6 @@ eventRouter
 eventRouter
   .route('/:id')
   .get(eventController.getEvent)
-  .delete(eventController.deleteEventByIdOrWeekday);
+  .delete(eventController.deleteEventByIdOrWeekDay);
 
 export { eventRouter };
